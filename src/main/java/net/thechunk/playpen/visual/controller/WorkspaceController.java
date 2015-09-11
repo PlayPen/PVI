21a141f521029175db795e29e9105a52e6888936
Sam Bloomberg <sam@redxdev.com>
2015-09-10 22:40:22 -0700

    started workspace controller

--- src/main/java/net/thechunk/playpen/visual/controller/WorkspaceController.java
+++ src/main/java/net/thechunk/playpen/visual/controller/WorkspaceController.java
@@ -1,6 +1,9 @@
 package net.thechunk.playpen.visual.controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import net.thechunk.playpen.visual.PVIApplication;
+import net.thechunk.playpen.visual.PVIClient;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,5 +11,9 @@ import java.util.ResourceBundle;
 public class WorkspaceController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if (PVIClient.get() == null) {
+            PVIApplication.get().showExceptionDialog("Exception Encountered", "PVIClient was not initialized when entering the workspace.", new Exception("PVIClient.get() returned null"));
+            PVIApplication.get().quit();
+        }
     }
 }
